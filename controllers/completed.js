4748d00806972ce4a93027ef36e3fde365ed9a14
Ashle3 <ashleeb3@byui.edu>
2023-11-17 00:00:35 -0600

    created completed post and put functions

---
+++ controllers/completed.js
@@ -0,0 +1,59 @@
+const mongodb = require('../database/connection');
+const ObjectId = require('mongodb').ObjectId;
+
+//GET ALL
+
+//GET SINGLE
+
+//POST
+const addCompletedPerson = async (req, res) => {
+    const completed = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        gender: req.body.gender,
+        birthday: req.body.birthday,
+        baptism: req.body.baptism,
+        confirmation: req.body.confirmation,
+        initiatory: req.body.initiatory,
+        endowment: req.body.endowment,
+        sealing: req.body.sealing
+    };
+    //db name subject to change after mongodb setup, is ttracker for now
+    const result = await mongodb.getDb().db('ttracker').collection('completed').insertOne(completed);
+    if(result.acknowledged){
+        res.status(201).json(result);
+    } else {
+        res.status(500).json(response.error || 'An error occurred while creating the person.');
+    }
+};
+
+//PUT
+const updateCompletedPerson = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid contact id to update a person.');
+    }
+    const personId = new ObjectId(req.params.id);
+    const updatedPerson = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        gender: req.body.gender,
+        birthday: req.body.birthday,
+        baptism: req.body.baptism,
+        confirmation: req.body.confirmation,
+        initiatory: req.body.initiatory,
+        endowment: req.body.endowment,
+        sealing: req.body.sealing
+    };
+    //db name subject to change after mongodb setup, is ttracker for now
+    const result = await mongodb.getDb().db('ttracker').collection('completed').replaceOne({ _id: personId }, updatedPerson);
+    console.log(result)
+    if(result.modifiedCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error occurred while updating the contact.');
+    }
+};
+
+//DELETE
+
+module.exports = { addCompletedPerson, updateCompletedPerson };
