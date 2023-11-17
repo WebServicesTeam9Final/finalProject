8957febd02699b67b03ae5e8668a6733778e6b8f
Ashle3 <ashleeb3@byui.edu>
2023-11-17 00:16:06 -0600

    fixed database folder misspelling

---
+++ controllers/temples.js
@@ -0,0 +1,14 @@
+const mongodb = require('../database/connection');
+const ObjectId = require('mongodb').ObjectId;
+
+//GET ALL
+
+//GET SINGLE
+
+//POST
+
+//PUT
+
+//DELETE
+
+module.exports = {  };
