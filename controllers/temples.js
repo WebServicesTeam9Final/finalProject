a50a5cbd6fbab82357ca1be6008fa12c6d8238c6
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-11-24 10:45:22 -0700

    Formatting code (indentation).

--- controllers/temples.js
+++ controllers/temples.js
@@ -110,45 +110,45 @@ const getAll = async (req, res, next) => {
   /////// POST ///////
 const addTemple = async (req, res) => {
   console.log(`temple records/POST document: `);
-    /*  #swagger.summary = 'Add a single temple record.'
-        #swagger.description = 'Adds a single temple record using information provided in a JSON body.'
-        #swagger.tags = ['Temples']
-        #swagger.parameters['record'] = {
-          in: 'body',
-          description: 'A valid JSON object with required data elements populated.',
-          type: 'object',
-          format: 'json',
-          schema: {
-            "templeId": "1",
-            "templeName": "Mount Timpanogos Utah Temple",
-            "templeAddress": "742 N 900 E, American Fork, UT 84003, USA"
-          }
+  /*  #swagger.summary = 'Add a single temple record.'
+      #swagger.description = 'Adds a single temple record using information provided in a JSON body.'
+      #swagger.tags = ['Temples']
+      #swagger.parameters['record'] = {
+        in: 'body',
+        description: 'A valid JSON object with required data elements populated.',
+        type: 'object',
+        format: 'json',
+        schema: {
+          "templeId": "1",
+          "templeName": "Mount Timpanogos Utah Temple",
+          "templeAddress": "742 N 900 E, American Fork, UT 84003, USA"
         }
-        #swagger.responses[201] = {
-          description: "Created - A single temple record is added with the data given. The return result provides the newly assigned ID number.",
-          schema: {
-            acknowledged: true,
-            insertedId: '<hexadecimal string>'
-          }
+      }
+      #swagger.responses[201] = {
+        description: "Created - A single temple record is added with the data given. The return result provides the newly assigned ID number.",
+        schema: {
+          acknowledged: true,
+          insertedId: '<hexadecimal string>'
         }
-        #swagger.responses[422] = {
-          description: 'Invalid or missing data error.'
-        }
-        #swagger.responses[500] = {
-          description: 'Internal server or database error.'
-        }
-    */
-    const temple = {
-        templeName: req.body.templeName,
-        templeAddress: req.body.templeAddress
-    };
-    //db name subject to change after mongodb setup
-    const result = await mongoDb.getDb().db('TempleWork').collection('temples').insertOne(temple);
-    if(result.acknowledged){
-        res.status(201).json(result);
-    } else {
-        res.status(500).json(response.error || 'An error occurred while creating the temple.');
-    }
+      }
+      #swagger.responses[422] = {
+        description: 'Invalid or missing data error.'
+      }
+      #swagger.responses[500] = {
+        description: 'Internal server or database error.'
+      }
+  */
+  const temple = {
+      templeName: req.body.templeName,
+      templeAddress: req.body.templeAddress
+  };
+  //db name subject to change after mongodb setup
+  const result = await mongoDb.getDb().db('TempleWork').collection('temples').insertOne(temple);
+  if(result.acknowledged){
+      res.status(201).json(result);
+  } else {
+      res.status(500).json(response.error || 'An error occurred while creating the temple.');
+  }
 };
 
   /////// PUT ///////
@@ -188,68 +188,68 @@ const updateTemple = async (req, res) => {
         #swagger.responses[500] = {
           description: "Internal server or database error.",
         }
-    */
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid id to update a temple.');
-    }
-    const templeId = new ObjectId(req.params.id);
-    const updatedTemple = {
-        templeName: req.body.templeName,
-        templeAddress: req.body.templeAddress
-    };
-    //db name subject to change after mongodb setup, is ttracker for now
-    const result = await mongoDb.getDb().db('TempleWork').collection('temples').replaceOne({ _id: templeId }, updatedTemple);
-    console.log(result)
-    if(result.modifiedCount > 0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'An error occurred while updating the temple.');
-    }
+  */
+  if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid id to update a temple.');
+  }
+  const templeId = new ObjectId(req.params.id);
+  const updatedTemple = {
+      templeName: req.body.templeName,
+      templeAddress: req.body.templeAddress
+  };
+  //db name subject to change after mongodb setup, is ttracker for now
+  const result = await mongoDb.getDb().db('TempleWork').collection('temples').replaceOne({ _id: templeId }, updatedTemple);
+  console.log(result)
+  if(result.modifiedCount > 0){
+      res.status(204).send();
+  } else {
+      res.status(500).json(response.error || 'An error occurred while updating the temple.');
+  }
 };
   
-  /////// DELETE ///////
-  const deleteData = async (req, res, next) => {
-    if (!ObjectId.isValid(req.params.id)) {
-      res.status(400).json('Must use a valid id to delete a temple.');
-    }
-    const templeId = new ObjectId(req.params.id);
-    const result = await mongoDb.getDb().db('TempleWork').collection('temples').deleteOne({ _id: templeId }, true);
-    console.log(result)
-    if(result.deletedCount > 0){
-        res.status(200).send();
-    } else {
-        res.status(500).json(response.error || 'An error occurred while deleting the temple.');
-    }
-    /*  #swagger.summary = 'Delete a single temple record.'
-        #swagger.description = 'Deletes a temple record identified by `id`. If `id` does not exist, no action is taken and no error occurs. Check the `deletedCount` attribute in the response to determine if a temple record was actually deleted.'
-        #swagger.tags = ['Temples']
-        #swagger.parameters['id'] = {
-          in: 'path',
-          description: 'A valid and unique 24-digit hexadecimal string that identifies a temple record.',
-          type: 'string',
-          format: 'hex',
-        } 
-        #swagger.responses[200] = {
-          description: "The temple record identified by `id` is deleted from the collection if it exists. The response is an object containing an aknowledgement and the number of matching temple record(s) deleted.",
-          schema: {
-            acknowledged: true,
-            deletedCount: 1
-          }
+/////// DELETE ///////
+const deleteData = async (req, res, next) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid id to delete a temple.');
+  }
+  const templeId = new ObjectId(req.params.id);
+  const result = await mongoDb.getDb().db('TempleWork').collection('temples').deleteOne({ _id: templeId }, true);
+  console.log(result)
+  if(result.deletedCount > 0){
+      res.status(200).send();
+  } else {
+      res.status(500).json(response.error || 'An error occurred while deleting the temple.');
+  }
+  /*  #swagger.summary = 'Delete a single temple record.'
+      #swagger.description = 'Deletes a temple record identified by `id`. If `id` does not exist, no action is taken and no error occurs. Check the `deletedCount` attribute in the response to determine if a temple record was actually deleted.'
+      #swagger.tags = ['Temples']
+      #swagger.parameters['id'] = {
+        in: 'path',
+        description: 'A valid and unique 24-digit hexadecimal string that identifies a temple record.',
+        type: 'string',
+        format: 'hex',
+      } 
+      #swagger.responses[200] = {
+        description: "The temple record identified by `id` is deleted from the collection if it exists. The response is an object containing an aknowledgement and the number of matching temple record(s) deleted.",
+        schema: {
+          acknowledged: true,
+          deletedCount: 1
         }
-        #swagger.responses[400] = {
-          description: "Invalid ID provided.",
-        }
-        #swagger.responses[500] = {
-          description: 'Internal server or database error.'
-        }
-    */
-  };
+      }
+      #swagger.responses[400] = {
+        description: "Invalid ID provided.",
+      }
+      #swagger.responses[500] = {
+        description: 'Internal server or database error.'
+      }
+  */
+};
   
   
-  module.exports = {
-    getAll,
-    getOne,
-    addTemple, 
-    updateTemple,
-    deleteData
-  };
+module.exports = {
+  getAll,
+  getOne,
+  addTemple, 
+  updateTemple,
+  deleteData
+};
