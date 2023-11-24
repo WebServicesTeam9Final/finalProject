02530af27c64b2855782cd217df1281608cd967d
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-11-24 10:53:40 -0700

    Formatting (indentation).

--- controllers/users.js
+++ controllers/users.js
@@ -97,17 +97,18 @@ const addUser = async (req, res) => {
         #swagger.responses[500] = {
           description: 'Internal server or database error.'
         }
-    */
-    const user = {
-        userName: req.body.userName,
-        userPassword: req.body.userPassword
-    };
-    const result = await mongoDb.getDb().db('TempleWork').collection('users').insertOne(user);
-    if(result.acknowledged){
-        res.status(201).json(result);
-    } else {
-        res.status(500).json(response.error || 'An error occurred while creating the user.');
-    }
+  */
+
+  const user = {
+      userName: req.body.userName,
+      userPassword: req.body.userPassword
+  };
+  const result = await mongoDb.getDb().db('TempleWork').collection('users').insertOne(user);
+  if(result.acknowledged){
+      res.status(201).json(result);
+  } else {
+      res.status(500).json(response.error || 'An error occurred while creating the user.');
+  }
 };
 
 //PUT
@@ -146,61 +147,64 @@ const updateUser = async (req, res) => {
         #swagger.responses[500] = {
           description: "Internal server or database error.",
         }
-    */
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid user id to update a user.');
-    }
-    const userId = new ObjectId(req.params.id);
-    const updatedUser = {
-        userName: req.body.userName,
-        userPassword: req.body.userPassword
-    };
-    const result = await mongoDb.getDb().db('TempleWork').collection('users').replaceOne({ _id: userId }, updatedUser);
-    console.log(result)
-    if(result.modifiedCount > 0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'An error occurred while updating the user.');
-    }
+  */
+
+  if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid user id to update a user.');
+  }
+  const userId = new ObjectId(req.params.id);
+  const updatedUser = {
+      userName: req.body.userName,
+      userPassword: req.body.userPassword
+  };
+  const result = await mongoDb.getDb().db('TempleWork').collection('users').replaceOne({ _id: userId }, updatedUser);
+  console.log(result)
+  if(result.modifiedCount > 0){
+      res.status(204).send();
+  } else {
+      res.status(500).json(response.error || 'An error occurred while updating the user.');
+  }
 };
   
 /////// DELETE ///////
 const deleteData = async (req, res, next) => {
-    if (!ObjectId.isValid(req.params.id)) {
-      res.status(400).json('Must use a valid id to delete user.');
-    }
-    const userId = new ObjectId(req.params.id);
-    const result = await mongoDb.getDb().db('TempleWork').collection('users').deleteOne({ _id: userId }, true);
-    console.log(result)
-    if(result.deletedCount > 0){
-        res.status(200).send();
-    } else {
-        res.status(500).json(response.error || 'An error occurred while deleting the user.');
-    }    
-    /*  #swagger.summary = 'Delete a single user record.'
-        #swagger.description = 'Deletes a user record identified by `id`. If `id` does not exist, no action is taken and no error occurs. Check the `deletedCount` attribute in the response to determine if a user record was actually deleted.'
-        #swagger.tags = ['Users']
-        #swagger.parameters['id'] = {
-          in: 'path',
-          description: 'A valid and unique 24-digit hexadecimal string that identifies a user record.',
-          type: 'string',
-          format: 'hex',
-        } 
-        #swagger.responses[200] = {
-          description: "The user record identified by `id` is deleted from the collection if it exists. The response is an object containing an aknowledgement and the number of matching user record(s) deleted.",
-          schema: {
-            acknowledged: true,
-            deletedCount: 1
-          }
+  /*  #swagger.summary = 'Delete a single user record.'
+      #swagger.description = 'Deletes a user record identified by `id`. If `id` does not exist, no action is taken and no error occurs. Check the `deletedCount` attribute in the response to determine if a user record was actually deleted.'
+      #swagger.tags = ['Users']
+      #swagger.parameters['id'] = {
+        in: 'path',
+        description: 'A valid and unique 24-digit hexadecimal string that identifies a user record.',
+        type: 'string',
+        format: 'hex',
+      } 
+      #swagger.responses[200] = {
+        description: "The user record identified by `id` is deleted from the collection if it exists. The response is an object containing an aknowledgement and the number of matching user record(s) deleted.",
+        schema: {
+          acknowledged: true,
+          deletedCount: 1
         }
-        #swagger.responses[400] = {
-          description: "Invalid ID provided.",
-        }
-        #swagger.responses[500] = {
-          description: 'Internal server or database error.'
-        }
-    */
-  };
+      }
+      #swagger.responses[400] = {
+        description: "Invalid ID provided.",
+      }
+      #swagger.responses[500] = {
+        description: 'Internal server or database error.'
+      }
+  */
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid id to delete user.');
+  }
+  const userId = new ObjectId(req.params.id);
+  const result = await mongoDb.getDb().db('TempleWork').collection('users').deleteOne({ _id: userId }, true);
+  console.log(result)
+  if(result.deletedCount > 0){
+      res.status(200).send();
+  } else {
+      res.status(500).json(response.error || 'An error occurred while deleting the user.');
+  }   
+
+  
+};
   
   
 
