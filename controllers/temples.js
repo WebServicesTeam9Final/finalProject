c429e1c1d9cf5597677ec9d762343bbc1cd354e4
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-11-24 10:47:43 -0700

    Change colleciton to global variable.

--- controllers/temples.js
+++ controllers/temples.js
@@ -144,7 +144,7 @@ const addTemple = async (req, res) => {
       templeAddress: req.body.templeAddress
   };
   //db name subject to change after mongodb setup
-  const result = await mongoDb.getDb().db('TempleWork').collection('temples').insertOne(temple);
+  const result = await mongoDb.getDb().db('TempleWork').collection(collection).insertOne(temple);
   if(result.acknowledged){
       res.status(201).json(result);
   } else {
@@ -199,7 +199,7 @@ const updateTemple = async (req, res) => {
       templeAddress: req.body.templeAddress
   };
   //db name subject to change after mongodb setup, is ttracker for now
-  const result = await mongoDb.getDb().db('TempleWork').collection('temples').replaceOne({ _id: templeId }, updatedTemple);
+  const result = await mongoDb.getDb().db('TempleWork').collection(collection).replaceOne({ _id: templeId }, updatedTemple);
   console.log(result)
   if(result.modifiedCount > 0){
       res.status(204).send();
@@ -214,7 +214,7 @@ const deleteData = async (req, res, next) => {
     res.status(400).json('Must use a valid id to delete a temple.');
   }
   const templeId = new ObjectId(req.params.id);
-  const result = await mongoDb.getDb().db('TempleWork').collection('temples').deleteOne({ _id: templeId }, true);
+  const result = await mongoDb.getDb().db('TempleWork').collection(collection).deleteOne({ _id: templeId }, true);
   console.log(result)
   if(result.deletedCount > 0){
       res.status(200).send();
