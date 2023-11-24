987c633529ea7cd5776f2a9bb9dcbedec5f461c7
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-11-24 10:46:41 -0700

    Formatting (indentation)

--- controllers/temples.js
+++ controllers/temples.js
@@ -6,108 +6,109 @@ const collection = 'temples';
 
 /////// GET ///////
 const getAll = async (req, res, next) => {
-    /*
-      #swagger.summary = 'Return the entire list of temple records.'
-      #swagger.description = 'Returns all temple records in the collection. No filtering is applied.'
-      #swagger.tags = ['Temples']
-      #swagger.responses[200] = {
-        description: "All temple records are successfully fetched and returned in an array.",
-        schema: [{
-            "_id": "a1b2c3d4e5f6a1b2c3d4e5f6",
-            "templeId": "1",
-            "templeName": "Mount Timpanogos Utah Temple",
-            "templeAddress": "742 N 900 E, American Fork, UT 84003, USA"
-          }]
+  /*
+    #swagger.summary = 'Return the entire list of temple records.'
+    #swagger.description = 'Returns all temple records in the collection. No filtering is applied.'
+    #swagger.tags = ['Temples']
+    #swagger.responses[200] = {
+      description: "All temple records are successfully fetched and returned in an array.",
+      schema: [{
+          "_id": "a1b2c3d4e5f6a1b2c3d4e5f6",
+          "templeId": "1",
+          "templeName": "Mount Timpanogos Utah Temple",
+          "templeAddress": "742 N 900 E, American Fork, UT 84003, USA"
+        }]
+    }
+    #swagger.responses[500] = {
+        description: 'Internal server or database error.'
       }
-      #swagger.responses[500] = {
-          description: 'Internal server or database error.'
-        }
-    */
-    console.log(`${collection}/GET ALL: `);
-    try {
-      const result = await mongoDb.getDb()
-        .db()
-        .collection(collection)
-        .find();
-      
-      result.toArray()
-        .then( (lists) => {
-          console.log(`    200 - OK`);
-          res.setHeader('Content-Type', 'application/json');  
-          res.status(200).json(lists); 
-        });
-    } catch (err) {
-      console.log(`    500 - ${err.name}: ${err.message}`);
-      res.status(500).send('Internal server or database error.');
-      return false;
-    }
-  };
-  
-  const getOne = async (req, res, next) => {
-    /*  #swagger.summary = 'Get a single temple record.'
-        #swagger.description = 'Returns the temple record identified by `id` for a single temple record that needs temple work completed.'
-        #swagger.tags = ['Temples']
-        #swagger.parameters['id'] = {
-          in: 'path',
-          description: 'A valid and unique 24-digit hexadecimal string that identifies a temple record that needs temple work completed.',
-          type: 'string',
-          format: 'hex'
-        }
-        #swagger.responses[200] = {
-          description: "A single temple record identified by `id` is successfully returned.",
-          schema: {
-            "_id": "a1b2c3d4e5f6a1b2c3d4e5f6",
-            "templeId": "1",
-            "templeName": "Mount Timpanogos Utah Temple",
-            "templeAddress": "742 N 900 E, American Fork, UT 84003, USA"
-          }
-        }
-        #swagger.responses[400] = {
-          description: 'Invalid ID provided.'
-        }
-        #swagger.responses[404] = {
-          description: "Not found.",
-        }
-        #swagger.responses[500] = {
-          description: 'Internal server or database error.'
-        }
-    */
-    const paddedId = req.params.id.padStart(24,'0');
-    console.log(`${collection}/GET document ${paddedId}:`);
-    if (!ObjectId.isValid(req.params.id)) {
-      console.log('    400 - Invalid ID provided.');
-      res.status(400).send('You must provide a valid ID (24-digit hexadecimal string).');
-      return false;
-    }
+  */
+  console.log(`${collection}/GET ALL: `);
+  try {
+    const result = await mongoDb.getDb()
+      .db()
+      .collection(collection)
+      .find();
     
-    const myObjId = new ObjectId(paddedId); 
-  
-    try {
-      const result = await mongoDb.getDb()
-        .db()
-        .collection(collection)
-        .findOne( {"_id": myObjId }
-      );
-        
-      if (result) {
+    result.toArray()
+      .then( (lists) => {
         console.log(`    200 - OK`);
         res.setHeader('Content-Type', 'application/json');  
-        res.status(200).json(result); 
-      } else {
-        console.log(`    404 - Not found.`);
-        if (!res.headersSent) {
-          res.setHeader('Content-Type', 'text/plain');  
-          res.status(404).send('Not found.');  
+        res.status(200).json(lists); 
+      });
+  } catch (err) {
+    console.log(`    500 - ${err.name}: ${err.message}`);
+    res.status(500).send('Internal server or database error.');
+    return false;
+  }
+};
+  
+const getOne = async (req, res, next) => {
+  /*  #swagger.summary = 'Get a single temple record.'
+      #swagger.description = 'Returns the temple record identified by `id` for a single temple record that needs temple work completed.'
+      #swagger.tags = ['Temples']
+      #swagger.parameters['id'] = {
+        in: 'path',
+        description: 'A valid and unique 24-digit hexadecimal string that identifies a temple record that needs temple work completed.',
+        type: 'string',
+        format: 'hex'
+      }
+      #swagger.responses[200] = {
+        description: "A single temple record identified by `id` is successfully returned.",
+        schema: {
+          "_id": "a1b2c3d4e5f6a1b2c3d4e5f6",
+          "templeId": "1",
+          "templeName": "Mount Timpanogos Utah Temple",
+          "templeAddress": "742 N 900 E, American Fork, UT 84003, USA"
         }
       }
-    } catch (err) {
-      console.log(`    500 - ${err}`);
-      res.status(500).send('Internal server or database error.');
-      return false;
-    }
-  };
+      #swagger.responses[400] = {
+        description: 'Invalid ID provided.'
+      }
+      #swagger.responses[404] = {
+        description: "Not found.",
+      }
+      #swagger.responses[500] = {
+        description: 'Internal server or database error.'
+      }
+  */
+  const paddedId = req.params.id.padStart(24,'0');
+  console.log(`${collection}/GET document ${paddedId}:`);
+  if (!ObjectId.isValid(req.params.id)) {
+    console.log('    400 - Invalid ID provided.');
+    res.status(400).send('You must provide a valid ID (24-digit hexadecimal string).');
+    return false;
+  }
   
-  /////// POST ///////
+  const myObjId = new ObjectId(paddedId); 
+
+  try {
+    const result = await mongoDb.getDb()
+      .db()
+      .collection(collection)
+      .findOne( {"_id": myObjId }
+    );
+      
+    if (result) {
+      console.log(`    200 - OK`);
+      res.setHeader('Content-Type', 'application/json');  
+      res.status(200).json(result); 
+    } else {
+      console.log(`    404 - Not found.`);
+      if (!res.headersSent) {
+        res.setHeader('Content-Type', 'text/plain');  
+        res.status(404).send('Not found.');  
+      }
+    }
+  } catch (err) {
+    console.log(`    500 - ${err}`);
+    res.status(500).send('Internal server or database error.');
+    return false;
+  }
+};
+
+  
+/////// POST ///////
 const addTemple = async (req, res) => {
   console.log(`temple records/POST document: `);
   /*  #swagger.summary = 'Add a single temple record.'
@@ -151,7 +152,7 @@ const addTemple = async (req, res) => {
   }
 };
 
-  /////// PUT ///////
+/////// PUT ///////
 const updateTemple = async (req, res) => {
   /*  #swagger.summary = 'Update a single temple record.'
         #swagger.description = 'Updates the temple record identified by `id` using information provided in a JSON body.'
