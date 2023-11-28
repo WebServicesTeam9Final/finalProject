3bcc3e2b0c042c31f6cb143efc7d94e3dc34bb95
Madi <lut19003@byui.edu>
2023-11-28 14:47:46 -0700

    added validation, error handeling, and I think oauth

---
+++ middleware/validate.js
@@ -0,0 +1,122 @@
+const Validator = require('validatorjs');
+
+const validator = (body, rules, customMessages, callback) => {
+    const validation = new Validator(body, rules, customMessages);
+    validation.passes(() => callback(null, true));
+    validation.fails(() => callback(validation.errors), false);
+}
+
+const validateFamily = (req, res, next) => {
+    const validationRules = {
+        fname: 'required|string|',
+        lname: 'required|string|',
+        gender: 'required|string|',
+        birthday: 'required|string|',
+        baptism: 'required|string|',
+        confirmation: 'required|string|',
+        initiatory: 'required|string|',
+        endowment: 'required|string|',
+        sealing: 'required|string|'
+    };
+    validator(req.body, validationRules, {}, (err, status) => {
+        if (!status){
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else{
+            next();
+        }
+    })
+};
+
+const validateTemple = (req, res, next) => {
+    const validationRules = {
+        templeId: 'required|int',
+        templeName: 'required|string',
+        templeAddress: 'required|string'
+    };
+    validator(req.body, validationRules, {}, (err, status) => {
+        if (!status){
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else{
+            next();
+        }
+    })
+};
+
+const validateUser = (req, res, next) => {
+    const validationRules = {
+        userName: 'required|string',
+        userPassword: 'required|string'
+    };
+    validator(req.body, validationRules, {}, (err, status) => {
+        if (!status){
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else{
+            next();
+        }
+    })
+};
+
+const validateCompleted = (req, res, next) => {
+    const validationRules = {
+        fname: 'required|string',
+        lname: 'required|string',
+        gender: 'required|string',
+        birthday: 'required|string',
+        baptism: 'required|string',
+        confirmation: 'required|string',
+        initiatory: 'required|string',
+        endowment: 'required|string',
+        sealing: 'required|string'
+    };
+    validator(req.body, validationRules, {}, (err, status) => {
+        if (!status){
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else{
+            next();
+        }
+    })
+};
+
+const validateId = (req, res, next) => {
+    const validationRule = {
+        id: 'required|alpha_num|size:24'
+      }
+      validator(req.params, validationRule, {}, (err, status) => {
+        if (!status) {
+          res.status(412).send({
+            success: false,
+            message: 'Validation failed',
+            data: err
+          });
+        } else {
+          next();
+        }
+      });
+};
+
+const { requiresAuth } = require('express-openid-connect');
+
+module.exports = {
+    validateFamily,
+    validateCompleted,
+    validateTemple,
+    validateUser,
+    validateId,
+    requiresAuth
+};
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require('express-openid-connect');
 
-// const auth = require('express-openid-connect');
+const config = {
+    //authorization configuration
+    authRequired: true,
+    auth0Logout: true,
+    secret: process.env.SESSION_SECRET,
+    baseURL: '',
+    clientID: '',
+    issuerBaseURL: ''
+};
+router.use(auth(config));
 
-// const config = {
-//     //authorization configuration
-// };
-// router.use(auth(config));
-// router.get('/', (req, res) =>{
-//     res.send(req.oidc.isAutheticated() ? 'Logged in' : 'Logged out');
-// })
+router.get('/', (req, res) =>{
+    res.send(req.oidc.isAutheticated() ? 'Logged in' : 'Logged out');
+})
 
 // Pull in root controller.
 const rootCtrl = require('../controllers');
