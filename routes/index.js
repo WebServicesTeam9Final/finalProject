039319b344bd0b47fba46df222864d3289f0b0e5
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-12-07 22:49:55 -0700

    Increase version and restructure routes.

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const tools = require('../tools');
 const { auth } = require('express-openid-connect');
-
-const config = {
-    //authorization configuration
-    authRequired: true,
-    auth0Logout: true,
-    secret: process.env.SESSION_SECRET,
-    baseURL: '',
-    clientID: '',
-    issuerBaseURL: ''
-};
-router.use(auth(config));
-
-router.get('/', (req, res) =>{
-    res.send(req.oidc.isAutheticated() ? 'Logged in' : 'Logged out');
-})
 
 // Pull in root controller.
 const rootCtrl = require('../controllers');
 
-// Pull in additional routers.
+// Load additional routers.
 const apiDocs = require('./swagger');
 const completed = require('./completed');
 const familyMembers = require('./familyMembers');
 const temples = require('./temples');
 const users = require('./users');
+
+// Authorization configuration
+const config = {
+    authRequired: true,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+router.use(auth(config));
+tools.log('OpenID Connect initialized.');
+
+router.get('/', (req, res) =>{
+  res.send(req.oidc.isAutheticated() ? 'Logged in' : 'Logged out');
+})
 
 // Establish routes.
 
