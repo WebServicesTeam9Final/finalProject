e0aa107a935c1208fb7d42d158b1bde7e083de65
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-12-08 22:13:04 -0700

    Protected 'write' routes with OAuth.

--- routes/temples.js
+++ routes/temples.js
@@ -2,13 +2,16 @@
 
 const routes = require('express').Router();
 const templesCtrl = require('../controllers/temples');
+const { requiresAuth } = require('express-openid-connect');
 // const validation = require("../middleware/validate");
 
-// Allow All Read/Write Access
+// Allow All Read Access
 routes.get('/', templesCtrl.getAll);
 routes.get('/:id', templesCtrl.getOne);
-routes.post('/', templesCtrl.addTemple);
-routes.put('/:id', templesCtrl.updateTemple);
-routes.delete('/:id', templesCtrl.deleteData);
+
+// Allow Authorized Write Access
+routes.post('/', requiresAuth(), templesCtrl.addTemple);
+routes.put('/:id', requiresAuth(), templesCtrl.updateTemple);
+routes.delete('/:id', requiresAuth(), templesCtrl.deleteData);
 
 module.exports = routes;
--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,18 @@
 
 const routes = require('express').Router();
 const usersCtrl = require('../controllers/users');
+const { requiresAuth } = require('express-openid-connect');
 // const validation = require("../middleware/validate");
 
-// Allow All Read/Write Access
+// Allow All Read Access
 
 // Doesn't appear we have a "GET ALL" planned for this collection.
 // routes.get('/', usersCtrl.getAll); 
 routes.get('/:id', usersCtrl.getOne);
-routes.post('/', usersCtrl.addUser);
-routes.put('/:id', usersCtrl.updateUser);
-routes.delete('/:id', usersCtrl.deleteData);
+
+// Allow Authorized Write Access
+routes.post('/', requiresAuth(), usersCtrl.addUser);
+routes.put('/:id', requiresAuth(), usersCtrl.updateUser);
+routes.delete('/:id', requiresAuth(), usersCtrl.deleteData);
 
 module.exports = routes;
